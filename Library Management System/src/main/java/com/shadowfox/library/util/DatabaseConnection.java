b79e4fde31303573670cfd972a8531858a5f9abe
Someswar Satpati <someswarsatpati8@gmail.com>
2026-05-30 15:27:19 +0530

    Update DatabaseConnection.java

--- Library Management System/src/main/java/com/shadowfox/library/util/DatabaseConnection.java
+++ Library Management System/src/main/java/com/shadowfox/library/util/DatabaseConnection.java
@@ -9,7 +9,7 @@ public class DatabaseConnection {
     // Default MySQL configuration
     private static final String URL = "jdbc:mysql://localhost:3306/library_db";
     private static final String USER = "root";
-    private static final String PASSWORD = "papai1512";
+    private static final String PASSWORD = "your_password";
 
     /**
      * Get a connection to the database.
