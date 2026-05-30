f022d4460893bbfdc2cafe1a6340f575ed86a550
Someswar Satpati <someswarsatpati8@gmail.com>
2026-05-30 15:28:17 +0530

    Update DatabaseConnection.java

--- Bank Account Management System/src/main/java/com/shadowfox/banking/management/DatabaseConnection.java
+++ Bank Account Management System/src/main/java/com/shadowfox/banking/management/DatabaseConnection.java
@@ -11,7 +11,7 @@ import java.util.Map;
 public class DatabaseConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/banking_db";
     private static final String USER = "root";
-    private static final String PASSWORD = "papai1512";
+    private static final String PASSWORD = "your_password";
 
     public Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
